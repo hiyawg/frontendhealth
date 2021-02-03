56511e3a157f37ddafb1c2b79e288b89a5e0b265
Bedlu Erpassa <bedluerpassa@Bedlus-MBP.home>
2021-02-03 16:28:36 -0500

    edit updated

--- src/app/admin/patients/edit-patient/edit-patient.component.ts
+++ src/app/admin/patients/edit-patient/edit-patient.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-patient',
@@ -8,48 +10,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EditPatientComponent {
   patientForm: FormGroup;
-  formdata = {
-    first: 'Pooja',
-    last: 'Sarma',
-    gender: 'Female',
-    mobile: '123456789',
-    age: '23',
-    email: 'test@example.com',
-    maritalStatus: '1',
-    bGroup: 'O+',
-    bPresure: '123',
-    sugger: '150',
-    injury: 'Fever',
-    address: '101, Elanxa, New Yourk',
-    dob: '1987-02-17T14:22:18Z',
-  };
-  constructor(private fb: FormBuilder) {
-    this.patientForm = this.createContactForm();
+  id: any;
+  formdata :any;
+  constructor(private http:HttpClient,private fb: FormBuilder,private actRoute:ActivatedRoute) {
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.formdata = this.createContactForm(this.id);
+    console.log('in the constructor')
+    console.log(this.formdata);
+    
+  }
+  ngOnInit() {
+    console.log('i am on ng on init');
+    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.createContactForm(this.id);
   }
   onSubmit() {
     console.log('Form Value', this.patientForm.value);
   }
-  createContactForm(): FormGroup {
-    return this.fb.group({
-      first: [
-        this.formdata.first,
-        [Validators.required, Validators.pattern('[a-zA-Z]+')],
-      ],
+  createContactForm(id:any) : any {
+    console.log(id)
+    console.log('this is the id');
+    const rooturl = 'http://localhost:8009/Patient';      
+     this.http.get(rooturl + '/' + id).subscribe(data => {        
+      this.formdata = data; 
+     this.patientForm = this.fb.group({
+      first:  [ this.formdata.first,[Validators.required]],
       last: [this.formdata.last],
       gender: [this.formdata.gender, [Validators.required]],
       mobile: [this.formdata.mobile, [Validators.required]],
       age: [this.formdata.age],
-      maritalStatus: [this.formdata.maritalStatus],
+      
       email: [
         this.formdata.email,
         [Validators.required, Validators.email, Validators.minLength(5)],
       ],
       bGroup: [this.formdata.bGroup],
       bPresure: [this.formdata.bPresure],
-      address: [this.formdata.address],
+      address: [this.formdata.address[0].address_detail],
       dob: [this.formdata.dob, [Validators.required]],
       sugger: [this.formdata.sugger],
       injury: [this.formdata.injury],
-    });
-  }
+                         
+        });
+     
+  })
+  
+  
+}
 }
