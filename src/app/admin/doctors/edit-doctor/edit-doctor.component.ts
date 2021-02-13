ba4956578ebd43d1261469e434133fe130afeab6
filagot <filagotshewafera@gmail.com>
2021-02-13 14:57:47 +0300

    newchange

--- src/app/admin/doctors/edit-doctor/edit-doctor.component.ts
+++ src/app/admin/doctors/edit-doctor/edit-doctor.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Doctor } from 'src/app/shared/security/doctor';
 @Component({
   selector: 'app-edit-doctor',
   templateUrl: './edit-doctor.component.html',
@@ -23,8 +24,13 @@ export class EditDoctorComponent {
     onSubmit() {
       console.log('Form Value', this.docForm.value);
       console.log(this.id);
-      const rooturl = 'http://localhost:8009/Doctor';      
-      this.http.put(rooturl + '/' + this.id,this.docForm.value).subscribe(data => {        
+      const {first,last,username,gender,dob,department,mobile,designation,
+        address,education,email,password,role} = this.docForm.value;
+  
+      const doctorData: Doctor = {first:first,last:last,username:username,gender:gender,dob:dob,department:department,mobile:mobile,designation:designation,
+        address:address,education:education,email:email,password:password,role: [role]};
+      const rooturl = 'http://localhost:8009/Update';      
+      this.http.put(rooturl + '/' + this.id,doctorData).subscribe(data => {        
              
         console.log(data);                    
          });
