5a2f05823f2a36f53d16fcf33fa41ae00874b908
Bedlu Erpassa <bedluerpassa@Bedlus-MBP.home>
2021-02-15 23:01:50 -0500

    hie

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/cliniva'));
+app.use(express.static('./dist/dark'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/cliniva/'}),
+    res.sendFile('index.html', {root: 'dist/dark/'}),
 );
 
 // Start the app by listening on the default Heroku port
