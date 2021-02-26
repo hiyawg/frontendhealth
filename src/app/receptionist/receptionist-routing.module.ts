aab80582db8b4a7a0902b8288b6b6b253d40436f
Bedlu Erpassa <bedluerpassa@Bedlus-MBP.home>
2021-02-25 21:16:46 -0500

    routings added

--- src/app/receptionist/receptionist-routing.module.ts
+++ src/app/receptionist/receptionist-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     },
     {
       path: 'register',
-      component: InquiryComponent,
+      component: RegisterpatientComponent,
     },
     {
       path: 'search',
